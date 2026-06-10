01770aaf41d731b083f21455d3af9ee15f446f9b
vannaenae <vanessaadetoro@gmail.com>
2026-06-10 02:44:55 +0100

    feat: rename app bundle ID to com.soulspce.harmoniqapp

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.Soulspce.harmoniq.app',
+  appId: 'com.soulspce.harmoniqapp',
   appName: 'Harmoniq',
   webDir: 'dist',
   server: {
